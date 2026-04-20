bfb27dac78fc56c57ba7c54bfaff5c8e9cef899d
Benciel Cabutin <cabutinb@gmail.com>
2026-04-21 04:08:27 +0800

    starter files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="underline text-2xl">
+        This is a Movie List Web app with add and remove of favorites
+      </h1>
+    </div>
+  );
+}
+
+export default App;
