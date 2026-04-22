fafc94fe5bd5f0e86d1b74403d176b088bd645e4
Benciel Cabutin <cabutinb@gmail.com>
2026-04-23 00:22:13 +0800

    Fixed the UI of stars

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -15,7 +15,7 @@ function Favorites() {
           <span className="text-lg font-semibold truncate xl:text-2xl">
             Inception
           </span>
-          <div className="flex flex-row gap-2">
+          <div className="flex flex-row gap-2 items-center">
             <span>8.4</span>
             <span className="text-lg">
               <FaStar className="text-yellow-400" />
@@ -26,7 +26,7 @@ function Favorites() {
           <span className="text-lg font-semibold truncate xl:text-2xl">
             Shutter Island
           </span>
-          <div className="flex flex-row gap-2">
+          <div className="flex flex-row gap-2  items-center">
             <span>9.1</span>
             <span className="text-lg">
               <FaStar className="text-yellow-400" />
@@ -35,7 +35,7 @@ function Favorites() {
         </li>
         <li className="border-b border-b-border-bottom w-full flex flex-row justify-between px-2 py-4 cursor-pointer hover:bg-bg-main-hover transition-all duration-200 ease-in lg:px-4 xl:px-12 xl:text-2xl">
           <span className="text-lg font-semibold xl:text-2xl">Forgotten</span>
-          <div className="flex flex-row gap-2">
+          <div className="flex flex-row gap-2  items-center">
             <span>7.9</span>
             <span className="text-lg">
               <FaStar className="text-yellow-400" />
@@ -46,7 +46,7 @@ function Favorites() {
           <span className="text-lg font-semibold xl:text-2xl">
             Interstellar
           </span>
-          <div className="flex flex-row gap-2">
+          <div className="flex flex-row gap-2  items-center">
             <span>8.7</span>
             <span className="text-lg">
               <FaStar className="text-yellow-400" />
